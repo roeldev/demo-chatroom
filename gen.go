cc59f06a5917945c76d95a5f8fd8c1e217773075
roeldev <roeldev@users.noreply.github.com>
2025-09-19 16:45:02 +0200

    Remove obsolete gen.go

--- gen.go
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2025, Roel Schut. All rights reserved.
-// Use of this source code is governed by a GPL-style
-// license that can be found in the LICENSE file.
-
-//go:generate go run ./gen-dotenv.go
-
-package chatroom
